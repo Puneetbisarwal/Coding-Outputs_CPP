f92ce142e3655b4647bd15ca56a45cb7f45e0898
Puneetbisarwal <puneetbisarwal@gmail.com>
2024-10-11 18:21:27 +0530

    Map Code in C++

---
+++ Practice Outputs/Map_1.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+ int n;
+ cout << "What is the size of the array = ";
+ cin >> n;
+
+ cout << "Enter the array" << endl;
+
+ // Array is declared inside the Main Class
+ // Inside Main Class Size of Array can me 10^6 maximum
+ int arr[n];
+ for(int i=0;i<n;i++)
+		{
+			cin >> arr[i];
+		}
+
+ // pre-compute
+
+ map<int, int> mpp;
+ for(int i=0; i<n; i++)
+ {
+   mpp[arr[i]]++;
+ }
+
+ // iterate in the map
+  for(auto it : mpp)
+  {
+    cout << it.first << "->" << it.second << endl;
+  }
+
+ int t;
+ cout << "How many Queries are there = ";
+ cin>>t;
+ cout << "Enter the Queries = ";
+ while(t--)
+ {
+   int number;
+   cin >> number;
+
+   // fetch
+   cout << "Output = "<< mpp[number] << endl;
+
+ }
+return 0;
+
+}
